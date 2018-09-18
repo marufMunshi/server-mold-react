756ddd845145e870a33f3bb329a9e447fc262c6e
marufMunshi <marufhasanmunshi@gmail.com>
2018-09-18 19:35:57 +0600

    Footer changed and domain search reload another webpage

--- src/components/DomainSearch.js
+++ src/components/DomainSearch.js
@@ -1,30 +1,63 @@
 import React from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
 
-const DomainSearch = () => {
-    return (
-        <section id="domain-search">
-            <Grid>
-                <Row>
-                    <div className="search-area">
-                        <Col sm={12} md={4}>
-                            <h3 className="search-area__title">Domain Search</h3>
-                        </Col>
-                        <div className="search-area__form">
-                            <form>
-                                <Col sm={8} md={5} lg={6}>
-                                    <input type="text" placeholder="Domain Search"></input>
-                                </Col>
-                                <Col sm={4} md={3} lg={2}>
-                                    <button className="btn search-btn">Search</button>
-                                </Col>
-                            </form>
+class DomainSearch extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            domain: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        const domain = e.target.value;
+        this.setState(() => {
+            return {
+                domain
+            };
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        location.replace(`https://client.servermold.com/cart.php?a=add&domain=register&query=${this.state.domain}`)
+    }
+
+    render() {
+        return (
+            <section id="domain-search">
+                <Grid>
+                    <Row>
+                        <div className="search-area">
+                            <Col sm={12} md={4}>
+                                <h3 className="search-area__title">Domain Search</h3>
+                            </Col>
+                            <div className="search-area__form">
+                                <form onSubmit={this.handleSubmit}>
+                                    <Col sm={8} md={5} lg={6}>
+                                        <input 
+                                            type="text" 
+                                            name="domain" 
+                                            value={this.state.domain} 
+                                            placeholder="Domain Search"
+                                            onChange={this.handleChange}
+                                        >
+
+                                        </input>
+                                    </Col>
+                                    <Col sm={4} md={3} lg={2}>
+                                        <button type="submit" className="btn search-btn">Search</button>
+                                    </Col>
+                                </form>
+                            </div>
                         </div>
-                    </div>
-                </Row>
-            </Grid>
-        </section>
-    );
-};
+                    </Row>
+                </Grid>
+            </section>
+        );
+    }
+}
 
 export default DomainSearch;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,31 +6,19 @@ const Footer = () => {
         <footer id="footer">
             <Grid>
                 <Row>
-                    <div className="footer-img">
-                        <img src="/images/logo.png" alt="Server Mold" />
-                    </div>
-                    <Col xs={12} md={6} mdOffset={3}>
-                        <div className="footer-div">
-                            <h1 className="footer-div__title">Contact Us</h1>
-                            <div className="footer-div__details">
-                                <p><i className="ion-ios-telephone"></i> <span>+922487994</span></p>
-                                <p><i className="ion-chatbubble-working"></i><span>servermold@example.com</span></p>
-                                <p>
-                                    <i className="ion-social-facebook facebook-color">
-                                    </i>
-                                    <a className="facebook" href="#">facebook</a>
-                                    <i className="ion-social-twitter twitter-color">
-                                    </i>
-                                    <a href="#">twitter</a>
-                                </p>
-                            </div>
+                    <Col xs={12} md={6}>
+                        <div className="footer-links">
+                            <a target="blank" href="https://www.facebook.com/">Facebook</a>
+                            <a target="blank" href="https://www.twitter.com/">Twitter</a>
+                            <a href="#">Privacy Policy</a>
+                            <a href="#">Terms Of Condition</a>
                         </div>
+                    </Col>
+                    <Col xs={12} mdOffset={4} md={2}>
+                        <p><span>&copy;</span>Server Mold</p>
                     </Col>
                 </Row>
             </Grid>
-            <div className="copyright">
-                Copyright © 2018 by Server Mold. All rights reserved.
-                </div>
         </footer>
     );
 };
