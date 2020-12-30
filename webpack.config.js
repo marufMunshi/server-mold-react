1cc7c9820c4d3a795c4f7645160690b16b187e27
marufMunshi <marufhasanmunshi@gmail.com>
2020-12-30 11:32:47 +0600

    add engines in package.json. delete un-used files

--- webpack.config.js
+++ webpack.config.js
@@ -1,85 +1,98 @@
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const webpack = require('webpack');
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require("webpack");
 
-require('dotenv').config({ path: '.env'});
+require("dotenv").config({ path: ".env" });
 
 module.exports = (env) => {
-    const isProduction = env === 'production';
-    const CSSExtract = new ExtractTextPlugin('styles.css');
+  const isProduction = env === "production";
+  const CSSExtract = new ExtractTextPlugin("styles.css");
 
-    return {
-        entry: ['@babel/polyfill', './src/app.js'],
-        output: {
-            path: path.join(__dirname, 'public', 'dist'),
-            filename: 'bundle.js'
+  return {
+    entry: ["@babel/polyfill", "./src/app.js"],
+    output: {
+      path: path.join(__dirname, "public", "dist"),
+      filename: "bundle.js",
+    },
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/,
         },
-        module: {
-            rules: [
-                {
-                    loader: 'babel-loader',
-                    test: /\.js$/,
-                    exclude: /node_modules/
+        {
+          test: /\.s?css$/,
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: "css-loader",
+                options: {
+                  sourceMap: true,
                 },
-                {
-                    test: /\.s?css$/,
-                    use: CSSExtract.extract({
-                        use: [
-                            {
-                                loader: 'css-loader',
-                                options: {
-                                    sourceMap: true
-                                }
-                            },
-                            {
-                                loader: 'postcss-loader', options: {
-                                    sourceMap: true
-                                }
-                            },
-                            {
-                                loader: 'resolve-url-loader'
-                            },
-                            {
-                                loader: 'sass-loader',
-                                options: {
-                                    sourceMap: true
-                                }
-                            }
-                        ]
-                    })
+              },
+              {
+                loader: "postcss-loader",
+                options: {
+                  sourceMap: true,
                 },
-                {
-                    test: /\.(png|jpg|gif|svg)$/,
-                    use: [
-                        {
-                            loader: 'file-loader',
-                            options: {
-                                name: '[name].[ext]',
-                                outputPath: 'images/',
-                                publicPath: ''
-                            }
-                        }
-                    ]
-                }
-            ]
+              },
+              {
+                loader: "resolve-url-loader",
+              },
+              {
+                loader: "sass-loader",
+                options: {
+                  sourceMap: true,
+                },
+              },
+            ],
+          }),
         },
-        plugins : [
-            CSSExtract,
-            new webpack.DefinePlugin({
-                'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
-                'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-                'process.env.FIREBASE_DATABASE': JSON.stringify(process.env.FIREBASE_DATABASE),
-                'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-                'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-                'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
-            })
-        ],
-        devtool: isProduction ? 'source-map' : 'inline-source-map',
-        mode: isProduction ? 'production' : 'development',
-        devServer: {
-            contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true,
-            publicPath: '/dist/'
-        }
-    };
+        {
+          test: /\.(png|jpg|gif|svg)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "[name].[ext]",
+                outputPath: "images/",
+                publicPath: "",
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      CSSExtract,
+      new webpack.DefinePlugin({
+        "process.env.FIREBASE_API_KEY": JSON.stringify(
+          process.env.FIREBASE_API_KEY
+        ),
+        "process.env.FIREBASE_AUTH_DOMAIN": JSON.stringify(
+          process.env.FIREBASE_AUTH_DOMAIN
+        ),
+        "process.env.FIREBASE_DATABASE": JSON.stringify(
+          process.env.FIREBASE_DATABASE
+        ),
+        "process.env.FIREBASE_PROJECT_ID": JSON.stringify(
+          process.env.FIREBASE_PROJECT_ID
+        ),
+        "process.env.FIREBASE_STORAGE_BUCKET": JSON.stringify(
+          process.env.FIREBASE_STORAGE_BUCKET
+        ),
+        "process.env.FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(
+          process.env.FIREBASE_MESSAGING_SENDER_ID
+        ),
+      }),
+    ],
+    devtool: isProduction ? "source-map" : "inline-source-map",
+    mode: isProduction ? "production" : "development",
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      historyApiFallback: true,
+      publicPath: "/dist/",
+    },
+  };
 };
